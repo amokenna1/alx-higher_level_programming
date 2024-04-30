69b4700e2ee8b04c8f2a2ee8f5a731ecd2bbb011
amokenna1 <amokenna@icloud.com>
2024-04-30 10:19:35 -0700

    2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,18 +1,15 @@
-#!/usr/bin/python3
-""" Script that takes in a URL and an email, sends a POST request
- n displays the body of the response
- """
-
- import sys
- import urllib.parse
+ #!/usr/bin/python3
  import urllib.request
-
+ import urllib.parse
+ import sys
 
  if __name__ == "__main__":
     url = sys.argv[1]
-    value = {"email": sys.argv[2]}
-    data = urllib.parse.urlencode(value).encode("ascii")
+    email = sys.argv[2]
 
-    request = urllib.request.Request(url, data)
-    with urllib.request.urlopen(request) as resp:i
-        print(resp.read().decode("utf-8"))
+    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+    req = urllib.request.Request(url, data=data, method='POST')
+
+    with urllib.request.urlopen(req) as response:
+        print(response.read().decode('utf-8'))
+

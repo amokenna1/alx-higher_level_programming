82926b820adaa746e7de60d764a7d95c37bcde16
amokenna1 <amokenna@icloud.com>
2024-04-30 10:25:21 -0700

    2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,15 +1,18 @@
- #!/usr/bin/python3
- import urllib.request
- import urllib.parse
- import sys
+#!/usr/bin/python3
+"""Sends a POST request to a given URL with a given email.
+Usage: ./2-post_email.py <URL> <email>
+- Displays the body of the response.
+"""
+import sys
+import urllib.parse
+import urllib.request
 
- if __name__ == "__main__":
+
+if __name__ == "__main__":
     url = sys.argv[1]
-    email = sys.argv[2]
+    value = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(value).encode("ascii")
 
-    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
-    req = urllib.request.Request(url, data=data, method='POST')
-
-    with urllib.request.urlopen(req) as response:
-        print(response.read().decode('utf-8'))
-
+    request = urllib.request.Request(url, data)
+    with urllib.request.urlopen(request) as response:
+        print(response.read().decode("utf-8"))
